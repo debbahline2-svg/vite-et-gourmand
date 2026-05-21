90f1737999190e1f2d851abc661c59d59d44db51
aizawalyn-dot <debbahline2@gmail.com>
2026-05-21 11:15:15 +0200

    Fix port MySQL

--- includes/db.php
+++ includes/db.php
@@ -1,46 +1,33 @@
- <?php
-
+<?php
 class Database {
-
 private $host;
-
 private $db_name;
-
 private $username;
-
 private $password;
-
+private $port;
 public $conn;
 
-
 public function getConnection() {
-
 $this->host = getenv('MYSQLHOST');
-
 $this->db_name = getenv('MYSQLDATABASE');
-
 $this->username = getenv('MYSQLUSER');
-
 $this->password = getenv('MYSQLPASSWORD');
+$this->port = getenv('MYSQLPORT');
 
 $this->conn = null;
-
 try {
-
-$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-
+$this->conn = new PDO(
+"mysql:host=" . $this->host .
+";port=" . $this->port .
+";dbname=" . $this->db_name,
+$this->username,
+$this->password
+);
 $this->conn->exec("set names utf8");
-
 } catch(PDOException $e) {
-
 die("Erreur de connexion : " . $e->getMessage());
-
 }
-
 return $this->conn;
-
 }
-
 }
-
-?> 
+?>
