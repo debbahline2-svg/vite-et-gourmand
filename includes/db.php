22e5fe170eb6d4466a19a461ffb080978942d537
aizawalyn-dot <debbahline2@gmail.com>
2026-05-21 10:16:10 +0200

    Config DB pour Railway

--- includes/db.php
+++ includes/db.php
@@ -1,20 +1,46 @@
-<?php
-class Database {
-    private $host = "localhost";
-    private $db_name = "vite_et_gourmand";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+ <?php
 
-    public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
-        return $this->conn;
-    }
+class Database {
+
+private $host;
+
+private $db_name;
+
+private $username;
+
+private $password;
+
+public $conn;
+
+
+public function getConnection() {
+
+$this->host = getenv('MYSQLHOST');
+
+$this->db_name = getenv('MYSQLDATABASE');
+
+$this->username = getenv('MYSQLUSER');
+
+$this->password = getenv('MYSQLPASSWORD');
+
+$this->conn = null;
+
+try {
+
+$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+
+$this->conn->exec("set names utf8");
+
+} catch(PDOException $e) {
+
+die("Erreur de connexion : " . $e->getMessage());
+
 }
-?>
+
+return $this->conn;
+
+}
+
+}
+
+?> 
